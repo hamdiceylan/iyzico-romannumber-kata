18befde34ef9fad1df2fea25fdcd93d130bfc9c7
Lemi Orhan Ergin <lemi.ergin@iyzico.com>
2017-08-15 17:21:41 +0300

    converts 5

--- src/test/java/IntegerToRomanNumberConverterTest.java
+++ src/test/java/IntegerToRomanNumberConverterTest.java
@@ -12,8 +12,18 @@ public class IntegerToRomanNumberConverterTest {
         assertEquals("III", converter.convert(3));
     }
 
+    @Test
+    public void convert_5() throws Exception {
+        IntegerToRomanNumberConverter converter = new IntegerToRomanNumberConverter();
+        assertEquals("V", converter.convert(5));
+    }
+
     private class IntegerToRomanNumberConverter {
         public String convert(int input) {
+            if (input == 5) {
+                return "V";
+            }
+
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < input; i++) {
                 sb.append("I");
