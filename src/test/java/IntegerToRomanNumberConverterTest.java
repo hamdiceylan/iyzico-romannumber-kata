d0363f59660f8dee3b0c6b4f7383476142c72174
Lemi Orhan Ergin <lemi.ergin@iyzico.com>
2017-08-15 17:32:27 +0300

    refactored test classs

--- src/test/java/IntegerToRomanNumberConverterTest.java
+++ src/test/java/IntegerToRomanNumberConverterTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -7,9 +8,15 @@ import static org.junit.Assert.assertEquals;
 
 public class IntegerToRomanNumberConverterTest {
 
+    private IntegerToRomanNumberConverter converter;
+
+    @Before
+    public void doBefore() throws Exception {
+        converter = new IntegerToRomanNumberConverter();
+    }
+
     @Test
     public void convert_1_2_3() throws Exception {
-        IntegerToRomanNumberConverter converter = new IntegerToRomanNumberConverter();
         assertEquals("I", converter.convert(1));
         assertEquals("II", converter.convert(2));
         assertEquals("III", converter.convert(3));
@@ -17,7 +24,6 @@ public class IntegerToRomanNumberConverterTest {
 
     @Test
     public void convert_5_10_50() throws Exception {
-        IntegerToRomanNumberConverter converter = new IntegerToRomanNumberConverter();
         assertEquals("V", converter.convert(5));
         assertEquals("X", converter.convert(10));
         assertEquals("L", converter.convert(50));
@@ -25,7 +31,6 @@ public class IntegerToRomanNumberConverterTest {
 
     @Test
     public void convert_4() throws Exception {
-        IntegerToRomanNumberConverter converter = new IntegerToRomanNumberConverter();
         assertEquals("IV", converter.convert(4));
     }
 
