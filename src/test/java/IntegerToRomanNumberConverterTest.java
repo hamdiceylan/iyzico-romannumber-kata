ea629ffbe603d44d94e80080f00925c4dc0d344e
Lemi Orhan Ergin <lemi.ergin@iyzico.com>
2017-08-15 17:15:02 +0300

    converts 2

--- src/test/java/IntegerToRomanNumberConverterTest.java
+++ src/test/java/IntegerToRomanNumberConverterTest.java
@@ -5,14 +5,17 @@ import static org.junit.Assert.assertEquals;
 public class IntegerToRomanNumberConverterTest {
 
     @Test
-    public void convert_1() throws Exception {
+    public void convert_1_2() throws Exception {
         IntegerToRomanNumberConverter converter = new IntegerToRomanNumberConverter();
         assertEquals("I", converter.convert(1));
+        assertEquals("II", converter.convert(2));
     }
 
     private class IntegerToRomanNumberConverter {
         public String convert(int input) {
-            return "I";
+            if (input == 1)
+                return "I";
+            return "II";
         }
     }
 }
