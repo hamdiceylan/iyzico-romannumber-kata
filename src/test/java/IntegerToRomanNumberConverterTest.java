d1a75607b98eb328d40ae5d200bd40f102f804d1
Lemi Orhan Ergin <lemi.ergin@iyzico.com>
2017-08-15 17:20:04 +0300

    finalizes refactoring of the conversion for 1,2,3

--- src/test/java/IntegerToRomanNumberConverterTest.java
+++ src/test/java/IntegerToRomanNumberConverterTest.java
@@ -14,19 +14,11 @@ public class IntegerToRomanNumberConverterTest {
 
     private class IntegerToRomanNumberConverter {
         public String convert(int input) {
-            if (input >= 1) {
-                StringBuilder sb = new StringBuilder();
-                for (int i=0; i<input; i++) {
-                    sb.append("I");
-                }
-                return sb.toString();
-            } else {
-                if (input == 1)
-                    return "I";
-                else if (input == 2)
-                    return "II";
-                return "III";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input; i++) {
+                sb.append("I");
             }
+            return sb.toString();
         }
     }
 }
